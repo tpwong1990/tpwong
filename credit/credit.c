78be33f81c90e1a9b2b5f30145be4c8ab26fcc61
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-23 18:46:13 +0000

    Fri, Dec 23, 2022, 6:46 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,5 +5,11 @@ int main(void)
 {
     // ask user to input credit card number
     long card_no = get_long("Number: ");
-
+// AE: 34 or 37
+//Master: 51-55
+//Visa:4
+    for(int i = 16; i<0; i--)
+    {
+        if (card_no % 10)
+    }
 }
