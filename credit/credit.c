faeeb88f099165da228e4bedf03c90e6c31002a1
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-23 23:04:44 +0000

    Fri, Dec 23, 2022, 11:04 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -132,6 +132,9 @@ int extract_digit(long number, int no_digit, int target_digit)
             return digit;
             break;
         }
-
+        else
+        {
+            return null;
+        }
     }
 }
