6ecb97df2281390adaca7a81d66d6408508f867a
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-23 19:04:04 +0000

    Fri, Dec 23, 2022, 7:04 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <math.h>
 
+//check no of digit
+int digit_no_checking(long card_no_1);
+    int check_digit=0;
+    int i = 15.0;
+    do
+    {
+        printf("%i\n",i);
+        if (card_no_1 /(long)pow(10.0,i)>0)
+            {
+                check_digit = 1;
+            }
+        i--;
+    }
+    while(check_digit==0);
+    return i;
+
 int main(void)
 {
     // ask user to input credit card number
@@ -9,16 +25,6 @@ int main(void)
 // AE: 34 or 37
 //Master: 51-55
 //Visa:4
-int check_digit=0;
-int i = 15.0;
-    do
-    {
-        printf("%i\n",i);
-        if (card_no /(long)pow(10.0,i)>0)
-            {
-                check_digit = 1;
-            }
-        i--;
-    }
-    while(check_digit==0);
+    int j = digit_no_checking(card_no);
+    printf("%i\n",j)
 }
