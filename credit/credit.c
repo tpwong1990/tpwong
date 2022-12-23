de63cba2a3e442928dcf9467d23596330d38d1e0
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-23 18:42:45 +0000

    Fri, Dec 23, 2022, 6:42 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,11 +4,6 @@
 int main(void)
 {
     // ask user to input credit card number
-    long card_no;
-    //do
-    //{
+    long card_no = get_long("Number: ");
 
-        card_no = get_long("Number: ");
-    //} while(card_no >0);
-    printf(isdigit(card_no));
 }
