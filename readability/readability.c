10ff4ff4f3c5f6fe1de7ec7da0fb15e958a03b02
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-24 23:08:31 +0000

    Sat, Dec 24, 2022, 11:08 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,14 +4,19 @@
 int count_letters(string text);
 int count_words(string text);
 int count_sent(string text);
+int coleman_liau(int l, int w, int s);
 
 int main(void)
 {
     string s = get_string("Text: ");
     int letter_c = count_letters(s);
     int word_c = count_words(s);
+    int sent_c = count_sent(s);
     printf("%i\n", letter_c);
     printf("%i\n", word_c);
+    printf("%i\n", sent_c);
+    int c_l_index = coleman_liau(letter_c, word_c, sent_c);
+    printf("Grade %i\n", c_l_index);
 }
 
 int count_letters(string text)
@@ -54,4 +59,27 @@ int count_words(string text)
         i++;
     }
     return count;
+}
+
+int count_sent(string text)
+{
+    int i = 0;
+    int count = 0;
+    while (text[i] != '\0')
+    {
+        if (((text[i] == '!' || text[i] == '?') || text[i] == '.'))
+        {
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+
+int coleman_liau(int l, int w, int s)
+{
+    long ave_l = (long) l / w * 100;
+    long ave_s = (long) s / w * 100;
+    long index = 0.0588 * ave_l - 0.296 * ave_s -15.8;
+    return round(index);
 }
