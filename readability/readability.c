050b43d1d08ec73eb5f4421a29c9ee32cd1d11d7
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-24 14:46:14 +0000

    Sat, Dec 24, 2022, 2:46 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,14 +5,21 @@ int count_letters(string text);
 int main(void)
 {
     string s = get_string("Text: ");
+    int letter_c = count_letters(s);
+    printf("%i\n", letter_c);
 }
 
 int count_letters(string text)
 {
     int i = 0;
-    While (text[i] != /0)
+    int count = 0;
+    while (text[i] != '\0')
     {
-        if (text[i]>)
-
+        if ((text[i] >= 'A' && text[i] <= 'Z') || (text[i] >= 'a' && text[i] <= 'z'))
+        {
+            count++;
+        }
+        i++;
     }
+    return count;
 }
