de8a7363ef9082864867b7cbc273d314ae16b9b6
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-24 14:53:18 +0000

    Sat, Dec 24, 2022, 2:53 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 
 int count_letters(string text);
+int count_words(string text);
+int count_sent(string text);
+
 int main(void)
 {
     string s = get_string("Text: ");
     int letter_c = count_letters(s);
+    int word_c = count_words(s);
     printf("%i\n", letter_c);
+    printf("%i\n", word_c);
 }
 
 int count_letters(string text)
@@ -16,6 +21,21 @@ int count_letters(string text)
     while (text[i] != '\0')
     {
         if ((text[i] >= 'A' && text[i] <= 'Z') || (text[i] >= 'a' && text[i] <= 'z'))
+        {
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+
+int count_words(string text)
+{
+    int i = 0;
+    int count = 0;
+    while (text[i] != '\0')
+    {
+        if ((((text[i] == '!' || text[i] == '?') || (text[i] == ',' || text[i] == '.')) || text[i] == ' '))
         {
             count++;
         }
