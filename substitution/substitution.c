4dc3e3c144659a5ab5b90309ddce77538a4b32cb
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-25 15:36:13 +0000

    Sun, Dec 25, 2022, 3:36 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,6 +6,10 @@ int check_no_char_command(string key);
 int main(int argc, string argv[])
 {
     //check command line - no. of command line
+    if (argc != 1)
+    {
+        printf("Usage: ./substitution key\n");
+    }
     //check command line - no. of characters
     int no_char = check_no_char_command(argv[1]);
    if (no_char != 26)
