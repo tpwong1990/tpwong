d4a9eaf2e0b0c5fbd5674360674812163a1e415b
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-25 15:27:55 +0000

    Sun, Dec 25, 2022, 3:27 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,12 +6,13 @@ int main(int argc, string argv[])
     //check command line - no. of characters
    int i = 0;
    int l = 0;
-   printf("%c", argv[1]);
-   //while(argv[i] != '\0')
-   //{
+   string s = argv[1];
+   //printf("%s\n", s);
+   while(s[i] != '\0')
+   {
         l++;
         i++;
-   //}
+   }
    if (l != 26)
    {
     printf("Key must contain 26 characters.\n");
