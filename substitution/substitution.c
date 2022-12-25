e73957c0e7f2fdb4622186ad7fc96b75309144d5
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-25 17:58:11 +0000

    Sun, Dec 25, 2022, 5:58 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int check_no_char_command(string key);
+int check_char_command(string key);
 
 int main(int argc, string argv[])
 {
@@ -15,10 +16,20 @@ int main(int argc, string argv[])
     {
         //check command line - no. of characters
         int no_char = check_no_char_command(argv[1]);
+        int char_valid = check_char_command(argc[1]);
         if (no_char != 26)
         {
             printf("Key must contain 26 characters.\n");
-        return 2;
+            return 1;
+        }
+        if (char_valid == 0)
+        {
+            printf("The key should only cotain alphabetic characters.\n")
+            return 1;
+        }
+        if (char_valid == -1)
+        {
+            prinft("The key cannot contain )
         }
     }
 }
