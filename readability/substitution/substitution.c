c1b7270fa897b6c0de461a3cd100add5bbbf1274
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-25 15:14:30 +0000

    Sun, Dec 25, 2022, 3:14 PM +00:00

--- readability/substitution/substitution.c
+++ readability/substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    //check command line
+    if (argc =! 27)
+    {
+        printf(")
+    }
 }
