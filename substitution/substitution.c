0c84bbd0ac57442fba671854c0dca74cc262a590
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-25 15:33:06 +0000

    Sun, Dec 25, 2022, 3:33 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,21 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int check_no_char_command(string key);
+
 int main(int argc, string argv[])
 {
+    //check command line - no. of command line
     //check command line - no. of characters
-   int i = 0;
-   int l = 0;
-   string s = argv[1];
-   //printf("%s\n", s);
-   while(s[i] != '\0')
-   {
-        l++;
-        i++;
-   }
-   if (l != 26)
+    int no_char = check_no_char_command(argv[1]);
+   if (no_char != 26)
    {
     printf("Key must contain 26 characters.\n");
     return 1;
    }
+}
+int check_no_char_command(string key)
+{
+    int i = 0;
+    int l = 0;
+   while(key[i] != '\0')
+   {
+        l++;
+        i++;
+   }
+   return l;
 }
