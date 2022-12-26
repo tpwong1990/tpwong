d06ab5883ae3203f5b3ced4524b6fac674750b8e
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-26 21:41:44 +0000

    Mon, Dec 26, 2022, 9:41 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,10 +80,13 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int temp;
+    int temp = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-
+        if (candidate[i].vote > temp)
+        {
+            temp = candidate[i].vote;
+        }
     }
     return;
 }
