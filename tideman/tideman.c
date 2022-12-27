f4d2389960ac6d1355a488585f0b96bc09bef7e5
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-27 13:36:28 +0000

    Tue, Dec 27, 2022, 1:36 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -99,10 +99,12 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    for (int i = 0; i < )
-    if (strcmp(name, candidates[i]) == 0)
+    for (int i = 0; i < candidate_count; i++)
     {
-
+        if (strcmp(name, candidates[i]) == 0)
+        {
+            
+        }
     }
     return false;
 }
