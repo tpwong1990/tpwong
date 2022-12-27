47a355cfedb11402a44a31d57e8ab0bdf5b288d7
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-27 13:46:16 +0000

    Tue, Dec 27, 2022, 1:46 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -116,7 +116,11 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
+
     }
     return;
 }
