480910f580c8e168630e3f74d8bbe252e07b827e
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-27 13:45:26 +0000

    Tue, Dec 27, 2022, 1:45 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -114,7 +114,10 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return;
 }
 
