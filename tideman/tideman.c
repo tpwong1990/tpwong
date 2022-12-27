eb8ed5f74321e89388746563230df419fe6773c1
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-27 13:42:54 +0000

    Tue, Dec 27, 2022, 1:42 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -103,7 +104,8 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(name, candidates[i]) == 0)
         {
-            
+            ranks[i] = rank;
+            return true;
         }
     }
     return false;
