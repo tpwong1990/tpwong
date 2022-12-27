2030bd698c4c4082ecb5d14d137c314241291871
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-27 13:56:42 +0000

    Tue, Dec 27, 2022, 1:56 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -133,7 +133,13 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
+    }
     return;
 }
 
