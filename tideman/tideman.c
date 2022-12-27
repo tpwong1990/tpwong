7d11f967c06bcbe39426460f30d989a7502bb98b
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-27 17:13:03 +0000

    Tue, Dec 27, 2022, 5:13 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -127,14 +127,14 @@ void record_preferences(int ranks[])
             }
         }
     }
-    for (int i = 0; i < candidate_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            printf("%i", preferences[i][j]);
-        }
-        printf("\n");
-    }
+    //for (int i = 0; i < candidate_count; i++)
+    //{
+        //for (int j = 0; j < candidate_count; j++)
+        //{
+            //printf("%i", preferences[i][j]);
+        //}
+        //printf("\n");
+    //}
     return;
 }
 
