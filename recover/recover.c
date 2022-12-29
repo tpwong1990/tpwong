05fa703057deb69d1d39cd13778a29fe5ddc3077
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-29 23:43:07 +0000

    Thu, Dec 29, 2022, 11:43 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,5 +9,15 @@ int main(int argc, char *argv[])
         printf("Usage: ./name of a forensic image\n");
         return 1;
     }
-    char *for_image = argv[optind];
+    char *image_name = argv[1];
+
+    //open file
+    FILE *image_in = fopen(image_name, "r");
+    if (image_in == NULL)
+    {
+        printf("Could not open %s.\n", image_name);
+        return 1;
+    }
+    //Read file and find JPEG 
+    fclose(inptr);
 }
