622caf4c5939791e0e7e7e1b1473e1969567ba00
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-29 16:21:17 +0000

    Thu, Dec 29, 2022, 4:21 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -42,6 +42,41 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    //allocate dump image memory
+    RGBTRIPLE(*image_dump)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    //Blur the image
+    for (int i = 1; i < height - 1; i++)
+    {
+        for (int j = 1; j < width - 1; j++)
+        {
+            BYTE temp_R = 0;
+            BYTE temp_G = 0;
+            BYTE temp_B = 0;
+            //average neighboring pixels
+            for (int k = -1; k <= 1; k++)
+            {
+                for (int l = -1; l <= 1; l++)
+                {
+                    temp_R = temp_R + image[i + k][j + l].rgbtRed;
+                    temp_G = temp_G + image[i + k][j + l].rgbtGreen;
+                    temp_B = temp_B + image[i + k][j + l].rgbtBlue;
+                }
+            }
+            //assgin the averaged value to image_dump
+            image_dump[i][j].rgbtRed = temp_R / 9;
+            image_dump[i][j].rgbtGreen = temp_G / 9;
+            image_dump[i][j].rgbtBlue = temp_B / 9;
+        }
+    }
+
+    // paste image_dump to image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = image_dump[i][j];
+        }
+    }
     return;
 }
 
