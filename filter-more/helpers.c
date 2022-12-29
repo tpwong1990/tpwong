b2944aa5b44c613e72734ae3b6dd05ff5b7065fb
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-29 15:45:05 +0000

    Thu, Dec 29, 2022, 3:45 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,7 +7,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-
+            //take ave of RGB value
+            int temp = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            *image[i][j] = temp;
         }
     }
     return;
