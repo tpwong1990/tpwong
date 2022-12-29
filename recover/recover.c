42eb558ff26aa24a9f7694cff3a23a1d61a9cba1
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-29 23:34:23 +0000

    Thu, Dec 29, 2022, 11:34 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    //check command line - no. of command line
+    if (argc != 2)
+    {
+        printf("Usage: ./name of a forensic image\n");
+        return 1;
+    }
 }
