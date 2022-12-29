6d125d8fb4fcabaa9a15e46b5fa038ab5175f68f
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-29 16:03:56 +0000

    Thu, Dec 29, 2022, 4:03 PM +00:00

---
+++ credit/helpers.c
@@ -0,0 +1,52 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //take ave of RGB value
+            BYTE temp = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            image[i][j].rgbtBlue = temp;
+            image[i][j].rgbtGreen = temp;
+            image[i][j].rgbtRed = temp;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height / 2; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // flip the pixel horizontally
+            BYTE temp_R = image[i][j].rgbtRed;
+            BYTE temp_G = image[i][j].rgbtGreen;
+            BYTE temp_B = image[i][j].rgbtBlue;
+            image[i][j].rgbtRed = image[height - i][j].rgbtRed;
+            image[i][j].rgbtGreen = image[height - i][j].rgbtGreen;
+            image[i][j].rgbtBlue = image[height - i][j].rgbtBlue;
+            image[height - i][j].rgbtRed = temp_R;
+            image[height - i][j].rgbtGreen = temp_G;
+            image[height - i][j].rgbtBlue = temp_B;
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
