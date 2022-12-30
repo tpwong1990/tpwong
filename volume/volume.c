61ede404462ae6bb6689fc9d8125c14f7ae46fcd
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-30 17:36:18 +0000

    Fri, Dec 30, 2022, 5:36 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -45,6 +45,8 @@ int main(int argc, char *argv[])
     BYTE2 sample[1];
     while (fread(sample, 1, 1, input) != 0)
     {
+        long temp_l = (long) sample[0];
+        printf("%l/n", temp_l);
         fwrite(sample, 1, 1, output);
     }
     // Close files
