52a96c544a39fbe5140105df34be80d0dfa582af
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2022-12-30 13:08:58 +0000

    Fri, Dec 30, 2022, 1:08 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -33,9 +33,10 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Copy header from input file to output file
+    fwrite()
 
-    // TODO: Read samples from input file and write updated data to output file
+    //Read samples from input file and write updated data to output file
 
     // Close files
     fclose(input);
