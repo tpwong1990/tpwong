2701ce5ea24ead160a00382fd3ed10ca251c51dc
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-02 18:09:08 +0000

    Mon, Jan 2, 2023, 6:09 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -78,9 +78,9 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p->parents[0] == NULL && p->parents[1] == NULL)
+    if (p == Null)
     {
-        free(p);
+        return;
     }
     else
     {
