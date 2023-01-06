80ea927a4c8d8c4d447e984cce20048d40d33c92
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-06 18:26:25 +0000

    Fri, Jan 6, 2023, 6:26 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -12,5 +12,5 @@ for i in range(height):
     print("#" * (i + 1), end = '')
     print("  ", end = '')
     print("#" * (i + 1), end = '')
-    print(" " * (height - i - 1), end = '')
+    print(" " * (height - i - 2), end = '')
     print()
