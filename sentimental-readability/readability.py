74fcec0580250d9a5c10d547e259bfcc6a922718
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-06 21:51:17 +0000

    Fri, Jan 6, 2023, 9:51 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,7 +3,17 @@ import cs50
 
 def main():
     texts = cs50.get_string("Text: ")
-
+    letter_c = letters_count(texts)
+    word_c = words_count(texts)
+    sent_c = sent_count(texts)
+    cl_index = coleman_liau(letter_c, word_c, sent_c)
+    if cl_index >= 16:
+        print("Grade 16+")
+    else:
+        if (cl_index < 1):
+            print("Before Grade 1")
+        else:
+            print("Grade ", cl_index)
 
 def letters_count(s):
     count = 0
@@ -19,10 +29,14 @@ def words_count(s):
     for i in range(len(s)):
         if s[i] == ' ':
             if ((s[i - 1] == '!' or s[i - 1] == '?' or s[i - 1] == ',' or s[i - 1] == '.' or s[i - 1] == '"' or s[i - 1] == ':') and i > 1):
+                # dump action
+                dump = 1
             else:
                 count += 1
         else:
             if (s[i] == '!' or s[i] == '?' or s[i] == ',' or s[i] == '.' or s[i] == '"' or s[i] == ':'):
+                # dump action
+                dump = 1
             else:
                 count += 1
     return count
@@ -36,7 +50,13 @@ def sent_count(s):
     return count
 
 def coleman_liau(l, w, s):
-    
+    ave_l = float(l) / float (w) * 100.0
+    ave_s = float(s) / float (w) * 100.0
+    index = 0.0588 * ave_l - 0.296 * ave_s - 15.8
+    x = round(index)
+    return x
 
+
+main()
 
 
