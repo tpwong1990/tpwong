17cc8a128b74798e83670b3ddffc9e5bce080936
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-06 21:42:32 +0000

    Fri, Jan 6, 2023, 9:42 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,10 +5,32 @@ def main():
     texts = cs50.get_string("Text: ")
 
 
-def letter_count(s):
+def letters_count(s):
     count = 0
     s = s.lower()
     for i in range(len(s)):
         if (s(i) >= "a" and s(i) <= 'a'):
             count += 1
     return count
+
+
+def words_count(s):
+    count = 0
+    for i in range(len(s)):
+        if s[i] == ' ':
+            if ((s[i - 1] == '!' or s[i - 1] == '?' or s[i - 1] == ',' or s[i - 1] == '.' or s[i - 1] == '"' or s[i - 1] == ':') and i > 1):
+            else:
+                count += 1
+        else:
+            if (s[i] == '!' or s[i] == '?' or s[i] == ',' or s[i] == '.' or s[i] == '"' or s[i] == ':'):
+            else:
+                count += 1
+    return count
+
+
+def sent_count(s):
+    count = 0
+    for i in (range(len(s))):
+        
+
+
