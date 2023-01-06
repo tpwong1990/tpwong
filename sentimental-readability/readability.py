bf5b3d1c3a9e13b387987e5ee49117be071cbfb7
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-06 21:35:01 +0000

    Fri, Jan 6, 2023, 9:35 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,11 @@
-# TODO
+import cs50
+
+
+def main():
+    texts = cs50.get_string("Text: ")
+
+
+def letter_count(s):
+    count = 0
+    for i in range(len(s)):
+        if (s(i) >= "A" and s(i) <= 'Z') or 
