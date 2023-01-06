bc15eba5c6f1c46348831eeb5b1c47d84c3459f5
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-06 19:20:08 +0000

    Fri, Jan 6, 2023, 7:20 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,16 +5,33 @@ def main():
     # get credit card number
     card_no = cs50.get_string("Number: ")
     card_length = len(card_no)
-    print(card_no[0])
+    card_type = type_check(card_no)
+    if card_type == 1:
+        print("AMEX")
+    if card_type == 2:
+        print("MASTERCARD")
+    if card_type == 3:
+        print("VISA")
+    if card_type == 0:
+        print("INVALID")
 
 def type_check(no):
     # 0 = Invalid
     # 1 = AE
     # 2 = Master
     # 3 = Visa
+    type = 0
     valid = Luhn_check(no)
+    no_length = len(no)
+    first2_digit = no[0] + no[1]
     if valid == 1:
-        if 
+        if no_length == 15 and (first2_digit == "34" or first2_digit == "37"):
+            type = 1
+        if no_length == 16 and (int(first2_digit) >= 51 and int(first2_digit) <= 55):
+            type = 2
+        if (int(first2_digit) >= 40 and int(first2_digit) <= 49) and (no_length == 13 or no_length == 16):
+            type = 3
+    return type
 
 def Luhn_check(no):
     digit_temp = 0
