df22d251495e55bdc11ea240fb35d5b9958f65b9
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-06 19:00:49 +0000

    Fri, Jan 6, 2023, 7:00 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,4 +1,10 @@
 import cs50
 
-# get credit card number
-card_no = cs50.get_string("Number: ")
+
+def main:
+    # get credit card number
+    card_no = cs50.get_string("Number: ")
+    card_length = len(card_no)
+
+def no_length_check(length):
+    if length ==
