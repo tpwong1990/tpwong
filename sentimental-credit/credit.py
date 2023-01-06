22cc0cd9aca7171e35caf4e04fd8c055f24da4c7
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-06 19:11:56 +0000

    Fri, Jan 6, 2023, 7:11 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,10 +1,41 @@
 import cs50
 
 
-def main:
+def main():
     # get credit card number
     card_no = cs50.get_string("Number: ")
     card_length = len(card_no)
+    print(card_no[0])
 
-def no_length_check(length):
-    if length ==
+def type_check(no):
+    # 0 = Invalid
+    # 1 = AE
+    # 2 = Master
+    # 3 = Visa
+    valid = Luhn_check(no)
+    if valid == 1:
+        if 
+
+def Luhn_check(no):
+    digit_temp = 0
+    for i in range(len(no) + 1, 2):
+        tmp = int(no[i]) * 2
+        if tmp < 10:
+            digit_temp = digit_temp + tmp
+        else:
+            tmp1 = tmp / 10
+            tmp2 = tmp % 10
+            digit_temp = digit_temp + tmp1 + tmp2
+    for i in range(1, len(no) + 1, 2):
+        digit_temp = digit_temp + int(no(i))
+    if digit_temp % 10 == 0:
+        return 1
+    else:
+        return 0
+
+
+
+
+
+
+main()
