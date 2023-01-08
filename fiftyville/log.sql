6ba05cbf188af088c09f66b32b93b9cf1cfb398e
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 20:51:18 +0000

    Sun, Jan 8, 2023, 8:51 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street";
 -- to seef if there are any crime report on 28, July 2021 at Humphrey Street. The second report should be irrelevant to the crime.
+SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
