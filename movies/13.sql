debd5e0ada9c15b28cd636cb15d92271fac04dbf
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 20:26:03 +0000

    Sun, Jan 8, 2023, 8:26 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,5 +1,1 @@
- SELECT movie_id FROM stars JOIN people ON person_id = people.id WHERE people.name ="Kevin Bacon" AND people.birth = 1958;
-
- SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars JOIN people ON person_id = people.id WHERE people.name ="Kevin Bacon" AND people.birth = 1958));
-
- SELECT name FROM people JOIN stars ON id = stars.person_id WHERE movie_id IN (SELECT movie_id FROM people JOIN stars on id = stars.person_id WHERE name = "Kevin Bacon" AND birth = 1958);
+ SELECT name FROM people JOIN stars ON id = stars.person_id WHERE movie_id IN (SELECT movie_id FROM people JOIN stars on id = stars.person_id WHERE name = "Kevin Bacon" AND birth = 1958) AND name IN (SELECT name FROM people WHERE name != "Kevin Bacon");
