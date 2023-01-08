b3bb0021779672975a3ca2d3fefe6053438c4feb
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 20:32:54 +0000

    Sun, Jan 8, 2023, 8:32 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT(name) FROM people WHERE id IN (SELECT person_id FROM directors WHERE person_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0));
+SELECT DISTINCT(name) FROM people JOIN directors ON id = directors.person_id JOIN ratings ON directors.movie_id = ratings.movie_id WHERE rating >= 9.0;
