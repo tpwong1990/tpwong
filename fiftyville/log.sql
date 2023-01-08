12702f8f9981fcb2bcaf31a191f2e893e2b60d01
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 21:16:17 +0000

    Sun, Jan 8, 2023, 9:16 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,6 +5,5 @@ SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND 
 -- to see the transcript of witnesses
 SELECT id, hour, minute, license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND activity = "exit";
 -- based on the transcript of the withness (Ruth id:161, )to check the license plate no of the car which the thief drive and escape from the bakery car park
-
 SELECT name, license_plate, passport_number FROM people JOIN bank_accounts ON id = bank_accounts.person_id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw");
 -- to check the personal detail who had withdrawn some money on Leggett Street's ATM on 28, July, 2021
