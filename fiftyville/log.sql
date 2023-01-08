7c1108df5dc5148d817864e1dc2c05bddd3a7049
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 23:54:25 +0000

    Sun, Jan 8, 2023, 11:54 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -14,7 +14,7 @@ SELECT hour, minute, city FROM flights JOIN airports ON origin_airport_id = airp
 SELECT passport_number FROM passengers JOIN flights ON flight_id = flights.id WHERE year = 2021 AND month = 7 AND day = 29 AND hour = 8;
 -- (7th)get the passport number who take the first fligt on 29 July 2021 which the leave fiftyville
  SELECT name FROM people JOIN bank_accounts ON id = bank_accounts.person_id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND activity = "exit") AND passport_number IN (SELECT passport_number FROM passengers JOIN flights ON flight_id = flights.id WHERE year = 2021 AND month = 7 AND day = 29 AND hour = 8);
--- (8th)combine the passport result from 6th and 7th sql result.
+-- (8th)combine the passport result from 6th and 7th sql result, 
 SELECT name FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28);
 -- (9th)find the person who recieve a call on the crime day
 SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28);
