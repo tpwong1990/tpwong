02574d0b99a51e590141259b4b19422bf766fa56
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 21:52:24 +0000

    Sun, Jan 8, 2023, 9:52 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -12,4 +12,6 @@ SELECT name, passport_number FROM people JOIN bank_accounts ON id = bank_account
 SELECT hour, minute, city FROM flights JOIN airports ON origin_airport_id = airports.id WHERE year = 2021 AND month = 7 AND day = 29;
 -- to check the early flight details on 29 July 2021 based on the transcript of witnesses (Raymond)
 SELECT passport_number FROM passengers JOIN flights ON flight_id = flights.id WHERE year = 2021 AND month = 7 AND day = 29 AND hour = 8;
--- get the passport no who take the first fligt on 29 July 2021 which the leave fiftyville
+-- get the passport number who take the first fligt on 29 July 2021 which the leave fiftyville
+ SELECT name FROM people JOIN bank_accounts ON id = bank_accounts.person_id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND activity = "exit") AND passport_number IN (SELECT passport_number FROM passengers JOIN flights ON flight_id = flights.id WHERE year = 2021 AND month = 7 AND day = 29 AND hour = 8);
+-- combine the passport result from 6th and 7th sql result.
