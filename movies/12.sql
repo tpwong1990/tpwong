41668394fac8214a7d22d43cc80e32a09acbb3a2
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 16:09:57 +0000

    Sun, Jan 8, 2023, 4:09 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,3 +2,5 @@ SELECT id FROM people WHERE name = "Johnny Depp";
 SELECT id FROM people WHERE name = "Helena Bonham Carter";
 SElECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp");
 SElECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter");
+
+SELECT title FROM movies WHERE id IN (SElECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp"));
