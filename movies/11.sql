ba1f77963bf54093950cf21b9a72ee059bbeb75f
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 16:00:03 +0000

    Sun, Jan 8, 2023, 4:00 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN ratings ON id = movie_id WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name ="Chadwick Boseman")) ORDER BY rating DESC;
+SELECT title FROM movies JOIN ratings ON id = movie_id WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name ="Chadwick Boseman")) ORDER BY rating DESC LIMIT 5;
