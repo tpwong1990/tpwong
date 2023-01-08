17bd1478a87cd61432b0ffbd8fbe275cdd5d2624
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-08 21:01:07 +0000

    Sun, Jan 8, 2023, 9:01 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,5 @@ SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 A
 -- to see if there are any crime report on 28, July 2021 at Humphrey Street. The second report should be irrelevant to the crime.
 SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 -- to see the transcript of witnesses
+SELECT id, hour, minute, license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND activity = "exit";
+-- based on the transcript of the withness (Ruth id:161, )to check the license plate no of the car which the thief drive and escape from the bakery car park
