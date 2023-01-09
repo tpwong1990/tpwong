fd349aa340b5db05fa419740a2291a01f1f2775f
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-09 00:10:01 +0000

    Mon, Jan 9, 2023, 12:10 AM +00:00

--- fiftyville/9th.sql
+++ fiftyville/9th.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28);
+SELECT name FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration <= 60);
