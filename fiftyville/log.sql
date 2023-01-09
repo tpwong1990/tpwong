6bbeb1bdfe046eb5de42b0803e3f6b2b8707d29c
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-09 00:03:35 +0000

    Mon, Jan 9, 2023, 12:03 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -19,3 +19,5 @@ SELECT name FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls 
 -- (9th)find the person who recieve a call on the crime day
 SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28);
 -- (10th)find the person who make a call on the crime day
+SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28) AND name IN (SELECT name FROM people JOIN bank_accounts ON id = bank_accounts.person_id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw") AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND activity = "exit") AND passport_number IN (SELECT passport_number FROM passengers JOIN flights ON flight_id = flights.id WHERE year = 2021 AND month = 7 AND day = 29 AND hour = 8));
+-- (11th) combine the result from 10th sql and 8th sql
