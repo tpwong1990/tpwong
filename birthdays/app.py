4541791ea8713a9ad2d2d887528a9db380a8f04f
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-15 21:37:22 +0000

    Sun, Jan 15, 2023, 9:37 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -32,18 +32,18 @@ def index():
         # data validation
         #day31_month = [1, 3, 5, 7, 8, 10, 12]
         #day30_month = [4, 6, 9, 11]
-        #if name:
+        if name:
             #if (month in day31_month and (day > 0 and day < 32)) or (month in day30_month and (day > 0 and day < 31)) or (month == 2 and (day > 0 and day < 30)):
                 # correct day format
-                 #db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
-                 #return redirect("/")
+                 db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+                 return redirect("/")
             #else:
                 # incorrect day format
                 #return redirect("/")
-        #else:
+        else:
             # empty name
-            #return render_template("index.html")
-            return redirect("/")
+            return render_template("index.html")
+            #return redirect("/")
 
     else:
 
