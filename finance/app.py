0b0ea5becf9d84dc2e0752d490263049641598f0
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-15 23:11:01 +0000

    Sun, Jan 15, 2023, 11:11 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -119,7 +119,8 @@ def register():
     elif request.method == "POST":
         user_name = request.form.get("username")
         user_pw = request.form.get("password")
-        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", user_name, user_pw)
+        
+        db.execute("INSERT INTO users (username) VALUES(?, ?)", user_name, user_hash)
         return redirect("/")
     else:
         return apology("TODO")
