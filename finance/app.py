bfd8a94805efae9c6a050fd45cd867e63c496672
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-15 22:42:11 +0000

    Sun, Jan 15, 2023, 10:42 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -114,7 +114,8 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    return render_template("register.html")
+    #return apology("TODO")
 
 
 @app.route("/sell", methods=["GET", "POST"])
