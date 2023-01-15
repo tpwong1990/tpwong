63d6097a3cdeb0c101a95aeeb85e868d4c20ea06
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-15 23:08:42 +0000

    Sun, Jan 15, 2023, 11:08 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -119,14 +119,7 @@ def register():
     elif request.method == "POST":
         user_name = request.form.get("username")
         user_pw = request.form.get("password")
-        # check if the username is exist or not
-        #exist = db.execute("SELECT * FROM users WHERE username = ?", user_name)
-        #if exist:
-            # return error
-            #return apology("Username already exist")
-        #else:
-            # add user data to database
-            #db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", user_name, user_pw)
+            db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", user_name, user_pw)
             return redirect("/")
     else:
         return apology("TODO")
---
+++ finance/tmp.py
@@ -0,0 +1,7 @@
+        # check if the username is exist or not
+        #exist = db.execute("SELECT * FROM users WHERE username = ?", user_name)
+        #if exist:
+            # return error
+            #return apology("Username already exist")
+        #else:
+            # add user data to database
