b264515fcd0b55ada4d44040ff6c61d778cc1854
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-16 21:12:40 +0000

    Mon, Jan 16, 2023, 9:12 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -71,7 +71,12 @@ def buy():
             if current_cash[0]["cash"] >= cost:
                 # can buy
                 cash_temp = current_cash[0]["cash"] - cost
-                db.execute("INSERT ")
+                # check if the current stock exist in portfolio or not
+                portfolio = db.execute("SELECT symbol FROM portfolio WHERE user_id = ? AND symbol = ?", session["user_id"], symbol)
+                if not portfolio:
+                    db.execute("INSERT INTO portfolio (user_id, symbol, shares) VALUE(?, ?, ?)", session["user_id"], symbol, shares)
+                else:
+                    db.execute("")
                 return redirect("/")
             else:
             # cannot buy
