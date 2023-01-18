f479a1e729ebda2c631d7ba7b4380fd2c81f08d4
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-18 19:20:27 +0000

    Wed, Jan 18, 2023, 7:20 PM +00:00

--- CS50_final/app.py
+++ CS50_final/app.py
@@ -16,7 +16,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///expenses.db")
+db = SQL("sqlite:///expenses.db")
 
 @app.after_request
 def after_request(response):
