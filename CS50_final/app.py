0d46febf80ce615e00ad00ee565b354e3dfb0369
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-18 19:06:11 +0000

    Wed, Jan 18, 2023, 7:06 PM +00:00

--- CS50_final/app.py
+++ CS50_final/app.py
@@ -4,7 +4,7 @@ import datetime
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash\
+from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required
 
 # Configure application
--- finance/tmp.py
+++
@@ -1,7 +0,0 @@
-        # check if the username is exist or not
-        #exist = db.execute("SELECT * FROM users WHERE username = ?", user_name)
-        #if exist:
-            # return error
-            #return apology("Username already exist")
-        #else:
-            # add user data to database
