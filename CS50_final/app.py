94db460abdab0bd08e0a431e515566d386d4c85b
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-21 12:25:08 +0000

    Sat, Jan 21, 2023, 12:25 PM +00:00

--- CS50_final/app.py
+++ CS50_final/app.py
@@ -96,9 +96,9 @@ def login():
             return render_template("login.html")
 
         # check if username exist and pw is correct:
-        account = db.execute("SELECT * FROM users WHERE user_name = ?", username, pw)
+        account = db.execute("SELECT * FROM users WHERE user_name = ?", username)
         hash_check = account[0]["hash"]
-        if (not account) or (not check_password_hash(hash_check)):
+        if (not account) or (not check_password_hash(hash_check, pw)):
             flash("Username does not exist or password is not correct")
             return render_template("login.html")
 
