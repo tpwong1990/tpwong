932863c69e3c46b2657c257a4fb5e6adafdc229b
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-25 21:01:05 +0000

    Wed, Jan 25, 2023, 9:01 PM +00:00

--- CS50_final/app.py
+++ CS50_final/app.py
@@ -50,9 +50,11 @@ def index():
 def select():
     if request.method == "POST":
         delete_check = request.form.get("delete")
-        print(int(delete_check))
+        #print(int(delete_check))
         if int(delete_check) >0:
-            return redirect("/dataimport")
+            cursor = connection.cursor()
+            cursor.execute("DROP TABLE IF EXISTS tmp")
+            return redirect("/")
         # selected codition search
         selected_month = request.form.get("month")
         selected_year = request.form.get("year")
