47ecd05b656ac88f2e698c3b99a00d079fe21a3b
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-26 20:07:19 +0000

    Thu, Jan 26, 2023, 8:07 PM +00:00

--- CS50_final/app.py
+++ CS50_final/app.py
@@ -20,7 +20,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.config['UPLOAD_FOLDER'] = "/"
+app.config['UPLOAD_FOLDER'] = "/CS50_final/"
 
 @app.after_request
 def after_request(response):
