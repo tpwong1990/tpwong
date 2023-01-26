d9e06b379efbb47cebaf169c9e84e4da405c6367
tpwong1990 <120880877+tpwong1990@users.noreply.github.com>
2023-01-26 19:58:19 +0000

    Thu, Jan 26, 2023, 7:58 PM +00:00

--- CS50_final/app.py
+++ CS50_final/app.py
@@ -9,7 +9,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from helpers import login_required, check_integer, check_float
 
 # Configure application
